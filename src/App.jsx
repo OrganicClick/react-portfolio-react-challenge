6dce8f32f0f347d090d7ef92c50fe20a50c85ca9
Austin Luper <austin.luper@gmail.com>
2024-05-01 15:15:00 -0400

    Added PageContainer import and other relevant logic to my application into the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import PageContainer from './components/PageContainer';
+import './App.css';
 
 function App() {
+    // Define a state variable 'count' and a function 'setCount' to update it
   const [count, setCount] = useState(0)
 
   return (
     <div className="app-container">
-      {/* portfolio content goes here */}
+      <PageContainer />
     </div>
   )
 }
 
-export default App
+export default App;
