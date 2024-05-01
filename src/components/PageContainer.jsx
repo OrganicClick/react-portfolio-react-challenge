4b0e66df327416f2d90f290bf2106869d150200b
Austin Luper <austin.luper@gmail.com>
2024-05-01 14:37:47 -0400

    Began creating PageContainer () in the PageContainer.jsx file as well as variable to track the current page and set the default page of the application to the Portfolio.jsx

--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -6,3 +6,9 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
+
+const PageContainer = () => {
+    // Create a useState variable to hold the current page and set the default page to 'Portfolio'
+    const [currentPage, setCurrentPage] = useState('Portfolio');
+
+}
