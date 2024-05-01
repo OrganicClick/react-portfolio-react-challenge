47a92fc36c0ebff2fd135b0f1628b1a8c5db18de
Austin Luper <austin.luper@gmail.com>
2024-05-01 14:32:37 -0400

    Imported the useState React hook as well as the pages and components I need to render for this application, into the PageContainer.jsx file

---
+++ src/components/PageContainer.jsx
@@ -0,0 +1,8 @@
+// Import useState from 'react' and necessary components from their relative locations
+
+import { useState } from 'react';
+import Navbar from './Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
