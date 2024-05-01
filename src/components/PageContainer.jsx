a9d41bf67b88b5486c3fdf3b55570a1c0941605e
Austin Luper <austin.luper@gmail.com>
2024-05-01 14:54:50 -0400

    Created logic to export the PageContainer function and corrected the syntax of the PageContainer functions renderPage function and handlePageChange to be incorporated within the body of the PageContainer component

--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -1,7 +1,6 @@
 // Import useState from 'react' and necessary components from their relative locations
-
 import { useState } from 'react';
-import Navbar from './Navbar';
+import NavBar from './Navbar';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
@@ -10,29 +9,33 @@ import Resume from './pages/Resume';
 const PageContainer = () => {
     // Create a useState variable to hold the current page and set the default page to 'Portfolio'
     const [currentPage, setCurrentPage] = useState('Portfolio');
-      // Function to render the current page based on state
-     const renderPage = () => {
+
+    // Function to render the current page based on state
+    const renderPage = () => {
         switch (currentPage) {
-        case 'About':
-            return <About />;
-        case 'Contact':
-            return <Contact />;
-        case 'Portfolio':
-            return <Portfolio />;
-        case 'Resume':
-            return <Resume />;
-        default:
-            return null;
-    }
-  };
+            case 'About':
+                return <About />;
+            case 'Contact':
+                return <Contact />;
+            case 'Portfolio':
+                return <Portfolio />;
+            case 'Resume':
+                return <Resume />;
+            default:
+                return null;
+        }
+    };
+
+    // Function to handle page change
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    // Return the navbar and the page content
+    return (
+        <div>
+            <NavBar currentPage={currentPage} handlePageChange={handlePageChange}/>
+            {renderPage()}
+        </div>
+    );
 };
 
-  // Function to handle page change
-  const handlePageChange = (page) => setCurrentPage(page);
-
-  // Return the navbar and the page content
-  return (
-    <div>
-         <NavBar currentPage={currentPage} handlePageChange={handlePageChange}/>
-    </div>
-  );
+export default PageContainer;
