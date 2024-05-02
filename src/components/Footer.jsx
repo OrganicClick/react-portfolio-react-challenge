b79e206c4754132e7cad7651f7f24e9f425aba2c
Austin Luper <austin.luper@gmail.com>
2024-05-02 15:32:04 -0400

    Added logic to the footer, including the link to the Github and LinkedIn pages for myself

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <p>&copy; 2024 Your Name</p>
+                <nav>
+                    <ul>
+                        <li><a href="https://github.com/OrganicClick?tab=repositories">GitHub</a></li>
+                        <li><a href="https://www.linkedin.com/in/austin-luper-a99750141/">LinkedIn</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
