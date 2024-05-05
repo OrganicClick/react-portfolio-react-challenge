41d0a7c860094942866063457447fe2610341417
Austin Luper <austin.luper@gmail.com>
2024-05-05 19:39:20 -0400

    Added styling classes to the Portfolio.jsx file and defined these classes within the app.css file

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../App.css'; // Import the CSS file
 
 const Portfolio = () => {
   return (
@@ -10,7 +11,7 @@ const Portfolio = () => {
         <div className="portfolio-grid">
           {/* Application 1 */}
           <div className="portfolio-item">
-          <img src="src/assets/Module-10-Screenshot.png" alt="SVG Generator Application" className="app-image" />
+            <img src="src/assets/Module-10-Screenshot.png" alt="SVG Generator Application" className="app-image" />
             <div className="app-details">
               <h3 className="app-title">SVG Generator Application</h3>
               <p className="app-description">An Object-oriented programming, back-end application that generates SVG logos.</p>
@@ -36,7 +37,6 @@ const Portfolio = () => {
             </div>
           </div>
 
-
           {/* Application 3 */}
           <div className="portfolio-item">
             <img src="src/assets/Module-12-Screenshot.png" alt="Employee Tracker Application" className="app-image" />
@@ -45,12 +45,10 @@ const Portfolio = () => {
               <p className="app-description">The Employee Tracker allows users to manage a company's employee database by performing various operations such as viewing employees, roles, and departments, adding new employees, updating employee roles, and deleting employees.</p>
               {/* Links to deployed application and GitHub repository */}
               <div className="app-links">
-
                 <a href="https://github.com/OrganicClick/employee-tracker-sql-challenge" target="_blank" rel="noopener noreferrer" className="app-link">GitHub Repo</a>
               </div>
             </div>
           </div>
-
 
           {/* Application 4 */}
           <div className="portfolio-item">
@@ -60,27 +58,24 @@ const Portfolio = () => {
               <p className="app-description">The application is designed to simulate the functionality of an e-commerce platform by implementing models for products, categories, tags, and product tags.</p>
               {/* Links to deployed application and GitHub repository */}
               <div className="app-links">
-                
                 <a href="https://github.com/OrganicClick/e-commerce-back-end-challenge" target="_blank" rel="noopener noreferrer" className="app-link">GitHub Repo</a>
               </div>
             </div>
           </div>
 
-
-            {/* Application 5 */}
-            <div className="portfolio-item">
-              <img src="src/assets/Module-5-Screenshot.png" alt="Calendar Application" className="app-image" />
-              <div className="app-details">
-                <h3 className="app-title">Calendar Application</h3>
-                <p className="app-description">This is a simple calendar application that allows a user to save events for each hour of a typical working day (9am–5pm). This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.</p>
-                {/* Links to deployed application and GitHub repository */}
-                <div className="app-links">
-                  <a href="https://organicclick.github.io/Module-5-Challenge-Luper/" target="_blank" rel="noopener noreferrer" className="app-link">Visit App</a>
-                  <a href="https://github.com/OrganicClick/Module-5-Challenge-Luper" target="_blank" rel="noopener noreferrer" className="app-link">GitHub Repo</a>
-                </div>
+          {/* Application 5 */}
+          <div className="portfolio-item">
+            <img src="src/assets/Module-5-Screenshot.png" alt="Calendar Application" className="app-image" />
+            <div className="app-details">
+              <h3 className="app-title">Calendar Application</h3>
+              <p className="app-description">This is a simple calendar application that allows a user to save events for each hour of a typical working day (9am–5pm). This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.</p>
+              {/* Links to deployed application and GitHub repository */}
+              <div className="app-links">
+                <a href="https://organicclick.github.io/Module-5-Challenge-Luper/" target="_blank" rel="noopener noreferrer" className="app-link">Visit App</a>
+                <a href="https://github.com/OrganicClick/Module-5-Challenge-Luper" target="_blank" rel="noopener noreferrer" className="app-link">GitHub Repo</a>
               </div>
             </div>
-
+          </div>
 
           {/* Application 6 */}
           <div className="portfolio-item">
@@ -95,7 +90,6 @@ const Portfolio = () => {
               </div>
             </div>
           </div>
-
 
         </div>
       </section>
