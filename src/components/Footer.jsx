c7eb3722c3b2ceacf9256b74b7d6af7dbd53c095
Austin Luper <austin.luper@gmail.com>
2024-05-05 16:31:20 -0400

    Added necessary styling for the footer so that the links display left to right as opposed to being stacked vertically

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="container">
-                <p>&copy; 2024 OrganicClick</p>
-                <nav>
-                    <ul>
-                        <li><a href="https://github.com/OrganicClick?tab=repositories">GitHub</a></li>
-                        <li><a href="https://www.linkedin.com/in/austin-luper-a99750141/">LinkedIn</a></li>
-                        <li><a href="https://stackoverflow.com/users/24842190/austin-luper">Stack Overflow</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; 2024 OrganicClick</p>
+        <nav>
+          <ul>
+            <li><a href="https://github.com/OrganicClick?tab=repositories">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/austin-luper-a99750141/">LinkedIn</a></li>
+            <li><a href="https://stackoverflow.com/users/24842190/austin-luper">Stack Overflow</a></li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
