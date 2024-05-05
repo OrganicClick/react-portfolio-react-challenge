ef54aba7d4c58adacaf945d3850354ec30531fb0
Austin Luper <austin.luper@gmail.com>
2024-05-05 16:02:56 -0400

    Added styling to clean up and professionalize the image of the Resume.jsx file

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 
 const Resume = () => {
   return (
-    <div>
+    <div className="resume-container">
       {/* Resume Content */}
-      <section>
-        <h2>Resume</h2>
-        <p>Download my resume <a href="/Module-20-Resume.pdf" download>here</a>.</p>
+      <section className="resume-section">
+        <h2 className="resume-heading">Resume</h2>
+        <p className="resume-download">Download my resume <a href="/Module-20-Resume.pdf" download className="resume-link">here</a>.</p>
         
         {/* List of Proficiencies */}
-        <h3>Proficiencies</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          {/* Add more proficiencies as needed */}
-        </ul>
+        <div className="proficiencies-container">
+          <h3 className="proficiencies-heading">Proficiencies</h3>
+          <ul className="proficiencies-list">
+            <li className="proficiency-item">HTML</li>
+            <li className="proficiency-item">CSS</li>
+            <li className="proficiency-item">JavaScript</li>
+            <li className="proficiency-item">GitHub</li>
+            <li className="proficiency-item">React</li>
+            <li className="proficiency-item">Mongoose</li>
+            <li className="proficiency-item">MongoDB</li>
+            <li className="proficiency-item">Insomnia</li>
+            {/* Add more proficiencies as needed */}
+          </ul>
+        </div>
       </section>
     </div>
   );
