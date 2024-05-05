41ba22bb1a0b7044a5bc9c9cbe94e18f353bab8f
Austin Luper <austin.luper@gmail.com>
2024-05-05 12:40:42 -0400

    Reformatted the content of the About.jsx page to look cleaner and more professional

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,18 +5,18 @@ import profilePicture from '../assets/profile-picture.jpg'; // Update the import
 const About = () => {
     return (
         <div className='about-container'>
-            <h1 className='about-title'>About Me</h1>
+            <div className='profile-picture'>
+                <img src={profilePicture} alt='Profile' className='profile-img' />
+            </div>
             <div className='about-content'>
-                <div className='profile-picture'>
-                    <img src={profilePicture} alt='Profile' className='profile-img' />
-                </div>
-                <div className='bio'>
-                    <p>
-                        Insert short bio here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                        ullamcorper, velit ac feugiat bibendum, turpis elit malesuada libero, sit amet eleifend
-                        urna risus id neque.
-                    </p>
-                </div>
+                <h1 className='about-title'>About Me</h1>
+                <p className='about-bio'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper, velit ac feugiat bibendum,
+                    turpis elit malesuada libero, sit amet eleifend urna risus id neque. Nulla facilisi. Suspendisse potenti.
+                    In hac habitasse platea dictumst. Integer tincidunt imperdiet magna, nec eleifend nunc fermentum eget.
+                    Donec ultricies augue vitae felis rutrum, at congue nulla posuere. Sed feugiat dui non purus dictum
+                    dignissim. Integer euismod dapibus eros ac fringilla.
+                </p>
             </div>
         </div>
     );
